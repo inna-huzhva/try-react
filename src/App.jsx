e193276c70111c55cef7275668b1ca92cd2c8aed
Inna Huzhva <guzhva.inna@gmail.com>
2022-08-22 14:34:17 +0300

    Created delete buttons

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,33 @@ function App() {
     setThings([...things, e]);
   }
 
+  function deleteAll() {
+    setThings([]);
+  }
+
+  function deleteElement(i) {
+    const newThings = things.slice();
+    newThings.splice(i, 1);
+    setThings(newThings);
+  }
+
+  const deleteAllDisabled = things.length === 0;
+
   return (
     <>
       <Input addElement={addElement} />
-      <h3>Things:</h3>
+      <h3>
+        Things:
+        <button id="delete-all-button" disabled={deleteAllDisabled} onClick={deleteAll}>
+          Delete all elements
+        </button>
+      </h3>
       <ul id="list">
         {things.map((t, i) => (
-          <li className="thing" key={i}>{t}</li>
+          <li className="thing" key={i}>
+            {t}
+            <button onClick={() => deleteElement(i)}>Delete</button>
+          </li>
         ))}
       </ul>
     </>
